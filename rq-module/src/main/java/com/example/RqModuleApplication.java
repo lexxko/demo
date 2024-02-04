03e8e045de5c221cc3c06d531ebe0c449a95bafd
akozyrev <lexxko@ya.ru>
2024-02-04 18:47:09 +0300

    first commit

---
+++ rq-module/src/main/java/com/example/RqModuleApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RqModuleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RqModuleApplication.class, args);
+    }
+}
